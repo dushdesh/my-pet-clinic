a743fa54da31642fa4b3e6585d5609460ed4211b
Dushyant Deshmukh <dushyant.deshmukh@hp.com>
2020-01-24 20:30:12 -0500

    Display owners on index. Closes #21

--- pet-clinic-web/src/main/java/com/dushdesh/mypetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/dushdesh/mypetclinic/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package com.dushdesh.mypetclinic.controllers;
 
+import com.dushdesh.mypetclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+    private final OwnerService ownerService;
 
-    @RequestMapping({"","/","index", "/index.html"})
-    public String getOwners() {
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"", "/", "index", "/index.html"})
+    public String getOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
