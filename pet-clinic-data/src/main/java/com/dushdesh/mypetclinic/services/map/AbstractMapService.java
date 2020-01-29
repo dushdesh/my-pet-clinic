f5b85a5954542f9f1e523aa27a540c6febd044f7
Dushyant Deshmukh <dushyant.deshmukh@hp.com>
2020-01-29 10:29:24 -0500

    Copy static resources. Closes #15

--- pet-clinic-data/src/main/java/com/dushdesh/mypetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/dushdesh/mypetclinic/services/map/AbstractMapService.java
@@ -17,9 +17,9 @@ public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> 
 
     T save(T object) {
         if (object != null) {
-           if (object.getId() == null) {
-               object.setId(getNextId());
-           }
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
             map.put(object.getId(), object);
             return object;
         } else {
