301c04ee7a818ae3cdca8a3e7d84bf7a9692134c
Dushyant Deshmukh <dushyant.deshmukh@hp.com>
2020-02-06 10:01:35 -0500

    Speciality May Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/com/dushdesh/mypetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.dushdesh.mypetclinic.services;
+
+import com.dushdesh.mypetclinic.models.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/dushdesh/mypetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,31 @@
+package com.dushdesh.mypetclinic.services.map;
+
+import com.dushdesh.mypetclinic.models.Speciality;
+import com.dushdesh.mypetclinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public Speciality delete(Speciality speciality) {
+        return super.deleteById(speciality.getId());
+    }
+}
