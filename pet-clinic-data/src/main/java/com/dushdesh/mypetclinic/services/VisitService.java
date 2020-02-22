189ef2dd50b49e26271336066c47ba19bfafd7ca
Dushyant Deshmukh <dushyant.deshmukh@hp.com>
2020-02-22 17:38:47 -0500

    Add map service for visit. Closes #45

---
+++ pet-clinic-data/src/main/java/com/dushdesh/mypetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.dushdesh.mypetclinic.services;
+
+import com.dushdesh.mypetclinic.models.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/dushdesh/mypetclinic/services/map/VisitMapService.java
@@ -0,0 +1,35 @@
+package com.dushdesh.mypetclinic.services.map;
+
+import com.dushdesh.mypetclinic.models.Visit;
+import com.dushdesh.mypetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet() == null || visit.getPet().getId() == null || visit.getPet().getOwner() == null
+                || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Visit should have a pet and owner");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit delete(Visit visit) {
+        return super.deleteById(visit.getId());
+    }
+}
